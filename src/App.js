51cf2bc25ed16977060d63eb6b911469e1abc2d1
Syed Aziz <syedaziz27x@gmail.com>
2019-10-13 23:55:11 -0400

    created route for dif palettes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import { generatePalette } from "./colorHelper";
 import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
   render() {
     return (
       <Switch>
         <Route exact path="/" render={() => <h1>PALETTE L GOES HERE</h1>} />
-        <Route exact path="/palette/:id" render={() => <h1>IND Pal</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
       </Switch>
     );
-
-    // <Palette palette={generatePalette(seedColors[4])} />;
   }
 }
 
